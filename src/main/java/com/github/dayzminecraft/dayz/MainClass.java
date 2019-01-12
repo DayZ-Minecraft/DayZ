8460a8fc82a193cb9ca28f435d05eeab12ac6a41
HoBoS_TaCo <HoBoS-TaCo@users.noreply.github.com>
2019-01-12 20:28:05 +1000

    rename base files to dayz

--- src/main/java/com/github/dayzminecraft/dayz/MainClass.java
+++ src/main/java/com/github/dayzminecraft/dayz/MainClass.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.github.dayzminecraft.dayz;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MainClass.MODID, name = MainClass.NAME, version = MainClass.VERSION)
+public class MainClass
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "dayz";
+    public static final String NAME = "DayZ Minecraft";
+    public static final String VERSION = "0.1";
 
     private static Logger logger;
 
